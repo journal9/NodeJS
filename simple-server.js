1ca32fa11b3353c4b738476d557da8042885c98a
Deepika-J <step.strong.79@gmail.com>
2024-01-22 23:32:40 +0530

    app that fetch from server created

---
+++ simple-server.js
@@ -0,0 +1,27 @@
+const { default: axios } = require("axios");
+const express = require("express");
+const app = express();
+const port = process.env.port || 3000;
+
+async function fetchApi(req, res) {
+  try {
+    const apiRes = await axios.get(`https://reqres.in/api/users?delay=3`);
+    if (apiRes.data) {
+      console.log("received");
+    } else {
+      console.log("not received");
+    }
+    console.log(apiRes);
+    //   await res.json({data: req.data});
+    res.send({ fromCache: false, data: apiRes.data });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+app.get("/fetch", fetchApi);
+app.get("/", fetchApi);
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});

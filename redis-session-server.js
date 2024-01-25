9967c419807dd19c2931ed9a5c695f7600b8f1ad
Deepika-J <step.strong.79@gmail.com>
2024-01-25 23:59:45 +0530

    session functionality added in server redis

---
+++ redis-session-server.js
@@ -0,0 +1,84 @@
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const session = require("express-session");
+const RedisStore = require('connect-redis').default;
+const app = express();
+const port = process.env.port || 3000;
+const { createClient } = require("redis");
+
+const redisClient = createClient({
+    host: 'localhost',
+    port: 6379
+})
+
+app.use(cookieParser());
+
+app.use(session({
+    store: new RedisStore({ client: redisClient }),
+    secret: 'your_secret_key',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        secure: false, // if true only transmit cookie over https
+        httpOnly: false, // if true prevent client side JS from reading the cookie 
+        maxAge: 1000 * 15 // session max age in miliseconds
+    }
+  }));
+
+function welcomePage(req, res) {
+  if (req.session.visited) {
+    req.session.visited += 1;
+  } else {
+    req.session.visited = 1;
+  }
+  res.send({
+    message:
+      "you visited this page " + req.session.visited + " times , welcome!",
+  });
+}
+
+function dashboardPage(req, res) {
+  if (req.session.isLoggedIn) {
+    res.send({ message: "welcome tyo dashboard" });
+  } else {
+    res.redirect("/");
+  }
+}
+
+function loginPage(req, res) {
+  if (req.session.isLoggedIn) {
+    res.redirect("/dashboard")
+  } else {
+    req.session.isLoggedIn = true
+    res.send({ message: "successfully logged in user" });
+  }
+}
+
+function logoutPage(req, res) {
+    if (req.session.isLoggedIn) {
+      req.session.isLoggedIn = false
+      res.redirect('/')
+    } else {
+      res.send({ message: "already logged out" });
+    }
+  }
+
+  function leavePage(req, res) {
+    req.session.destroy(err => {
+        if (err) {
+            return console.log(err);
+        }
+        res.redirect("/")
+    });
+  }
+
+app.get("/", welcomePage);
+app.get("/login", loginPage);
+app.get("/dashboard", dashboardPage);
+app.get("/logout", logoutPage);
+app.get("/exit", leavePage);
+
+
+app.listen(port, () => {
+  console.log(`server started at port ${port}`);
+});
